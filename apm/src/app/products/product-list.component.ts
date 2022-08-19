24669650d418008645511083a925703e5cbd0cdd
jo-cor <jcordoba@outlook.com>
2022-08-19 17:16:31 -0500

    ngOnInit

--- apm/src/app/products/product-list.component.ts
+++ apm/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -6,7 +6,7 @@ import { IProduct } from './product';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{  
   pageTitle: string = `Product
     List`;
   imageWidth: number = 50;
@@ -45,6 +45,10 @@ export class ProductListComponent {
       imageUrl: 'assets/images/hammer.png',
     },
   ];
+
+  ngOnInit(): void {
+    console.log( '[[[debug]]] @ ngOnInit');
+  }
 
   shouldDisplayProducts(): boolean {
     return this.products.length > 0;

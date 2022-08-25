52d3bf50b790397cf94289af7aaa7bbec83b5443
jo-cor <jcordoba@outlook.com>
2022-08-25 16:00:35 -0500

    retrieving url's parameter

--- apm/src/app/product-detail/product-detail.component.ts
+++ apm/src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   //   selector: 'pm-product-detail', //this won't be needed because it is not nestable, but routed instead
@@ -6,12 +7,13 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-  @Input()
-  id!: number;
-
   pageTitle: string = 'Product Detail';
 
+  constructor(private route: ActivatedRoute) {}
+
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
     //Retrieve 'id' from the url and assign it to this.id
   }
 }
